a259131ee45c8134b8e9af84337fbddc49b68bfe
Dagim Asnake <dagimasnake1@gmail.com>
2023-04-23 10:52:36 -1200

    added paginator

--- twitter/views.py
+++ twitter/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from twitter.models import TweetList
 from twitter.forms import TweetForm
 from django.contrib import messages
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -19,7 +20,10 @@ def tweet(request):
 
 
 def home(request):
-    allTweets = TweetList.objects.all
+    allTweets = TweetList.objects.all()
+    paginator = Paginator(allTweets, 5)
+    page = request.GET.get('pg')
+    allTweets = paginator.get_page(page)
     return render((request), 'home.html', {"allTweets": allTweets})
 
 
@@ -31,7 +35,10 @@ def profile(request):
 
 
 def mytweets(request):
-    myTweets = TweetList.objects.all
+    myTweets = TweetList.objects.all()
+    paginator = Paginator(myTweets, 5)
+    page = request.GET.get('pg')
+    myTweets = paginator.get_page(page)
     return render((request), 'mytweets.html', {"myTweets": myTweets})
 
 
@@ -39,3 +46,10 @@ def deleteTweet(request, tweetId):
     tweet = TweetList.objects.get(pk=tweetId)
     tweet.delete()
     return redirect('/twitter/profile/mytweets')
+
+
+def index(request):
+    context = {
+        "index": "index page"
+    }
+    return render((request), 'index.html', context)
--- twitterClone/urls.py
+++ twitterClone/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from twitter import views as twitterViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('twitter/', include('twitter.urls')),
+    path('', twitterViews.index, name='index')
 ]
